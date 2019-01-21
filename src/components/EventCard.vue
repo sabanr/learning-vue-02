dc17b9c5e3ee7c909084c9277248296d9c75147e
Roberto Saban <roberto.saban@sweet.com.ar>
2019-01-21 20:43:38 -0300

    configuration

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
